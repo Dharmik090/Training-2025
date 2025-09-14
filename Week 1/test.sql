98329b056592747129d9e3a0efe50dcda66de45c
Dharmik090 <dharmikparmarpd@gmail.com>
2025-09-14 22:00:22 +0530

    Week 1 Assignement: joins & aggregate functions

--- Week 1/test.sql
+++ Week 1/test.sql
@@ -83,3 +83,42 @@ WHERE BookID = 101;
 DELETE FROM Authors
 WHERE AuthorID = 1;
 
+
+/*
+Joins Queries
+*/
+
+-- List all books with their authors
+SELECT b.title, a.name AS author_name
+FROM Books b
+JOIN Authors a ON b.author_id = a.author_id;
+
+-- List all borrowed books with borrower details
+SELECT br.borrower_id, b.title, br.borrow_date, br.return_date
+FROM Borrowers br
+JOIN Books b ON br.book_id = b.book_id;
+
+-- List authors and the number of books they have written
+SELECT a.name, COUNT(b.book_id) AS book_count
+FROM Authors a
+LEFT JOIN Books b ON a.author_id = b.author_id
+GROUP BY a.author_id, a.name;
+
+
+/*
+Aggregate Functions
+*/  
+
+-- Count total number of books
+SELECT COUNT(*) AS total_books
+FROM Books;
+
+-- Find the earliest and latest published book
+SELECT MIN(year_published) AS earliest_year,
+       MAX(year_published) AS latest_year
+FROM Books;
+
+-- Count currently borrowed books & not yet returned
+SELECT COUNT(*) AS currently_borrowed
+FROM Borrowers
+WHERE return_date IS NULL;
